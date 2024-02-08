49b179797c00c7f7ea1d681613c860f76579e178
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-08 12:22:22 +0100

    added lang imgs

--- src/components/cover/CoverCard.vue
+++ src/components/cover/CoverCard.vue
@@ -3,6 +3,17 @@ export default {
     name: 'CoverCard',
     props: {
         production: Object
+    },
+    computed: {
+        HasFlag() {
+            const flags = ['it', 'en'];
+            return flags.includes(this.production.original_language);
+        },
+
+        flagSrc() {
+            const url = new URL(`../../assets/img/${this.production.original_language}.png`, import.meta.url);
+            return url.href;
+        }
     }
 };
 </script>
@@ -11,9 +22,16 @@ export default {
     <ul>
         <li>{{ production.title || production.name }}</li>
         <li>{{ production.original_title || production.original_name }}</li>
-        <li>{{ production.original_language }}</li>
+        <li>
+            <img v-if="HasFlag" :src="flagSrc" :alt="production.original_language">
+            <span v-else>{{ production.original_language }}</span>
+        </li>
         <li>{{ production.vote_average }}</li>
     </ul>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+img {
+    max-width: 100px;
+}
+</style>

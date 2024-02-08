55a8e90e7c29eaa4110e4050153b433690849fad
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-08 11:43:41 +0100

    created first api invocation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <script>
-import { store } from '../data/store'
+import axios from 'axios';
+import { store } from '../data/store';
+import { api } from '../data/index';
 import SearchBar from './SearchBar.vue';
 export default {
     data: () => ({ store }),
@@ -7,10 +9,30 @@ export default {
     components: { SearchBar },
     methods: {
         searchMovies() {
-            console.log(store.filter)
+            if (!store.filter) {
+                store.movies = [];
+                return;
+            }
+
+            const { baseUri, language, apiKey } = api;
+
+            const params = {
+                query: store.filter,
+                api_key: apiKey,
+                language
+            }
+
+            axios.get(`${baseUri}/search/movie`, { params })
+                .then((res) => {
+                    store.movies = res.data.results;
+                })
+                .catch((err) => {
+                    console.error(err)
+                })
         },
+
         setTitleFilter(term) {
-            store.filter = term;
+            store.filter = term; t
         }
     }
 };

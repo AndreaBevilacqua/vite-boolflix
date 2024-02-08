166ff831e8841979b654dc3244aef1e1190bc95d
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-08 11:32:16 +0100

    installed sass and created emits

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,13 +1,24 @@
 <script>
+import { store } from '../data/store'
 import SearchBar from './SearchBar.vue';
 export default {
+    data: () => ({ store }),
     name: 'AppHeader',
-    components: { SearchBar }
+    components: { SearchBar },
+    methods: {
+        searchMovies() {
+            console.log(store.filter)
+        },
+        setTitleFilter(term) {
+            store.filter = term;
+        }
+    }
 };
 </script>
 
 <template>
-    <SearchBar />
+    <SearchBar @form-submit="searchMovies" @term-change="setTitleFilter" placeholder="Cerca film o serie tv"
+        buttonLabel="Cerca" />
 </template>
 
 <style lang="scss"></style>
--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,12 +1,19 @@
 <script>
 export default {
-    name: 'SearchBar'
+    name: 'SearchBar',
+    data: () => ({ term: '' }),
+    props: {
+        buttonLabel: String,
+        placeHolder: String
+    },
+    emits: ['form-submit', 'term-change']
 };
 </script>
 
 <template>
-    <form>
-        <input type="text" v-model.trim="term">
+    <form @submit.prevent="$emit('form-submit')">
+        <input type="text" @keyup="$emit('term-change', term)" v-model.trim="term" :placeholder="placeHolder">
+        <button>{{ buttonLabel }}</button>
     </form>
 </template>
 

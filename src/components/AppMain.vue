58e406ab0648f139bd5e80de8ecb087452bc4997
AndreaBevilacqua <andrebevi2000@gmail.com>
2024-02-08 11:51:22 +0100

    added main elements

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,23 @@
 <script>
+import { store } from '../data/store';
 import CoversContainer from './CoversContainer.vue';
 export default {
     name: 'AppMain',
-    components: { CoversContainer }
+    components: { CoversContainer },
+    data: () => ({ store })
 };
 </script>
 
 <template>
-    <CoversContainer />
+    <section>
+        <h2>Movies</h2>
+        <ul v-for="movie in store.movies" :key="movie.id">
+            <li>{{ movie.title }}</li>
+            <li>{{ movie.original_title }}</li>
+            <li>{{ movie.original_language }}</li>
+            <li>{{ movie.vote_average }}</li>
+        </ul>
+    </section>
 </template>
 
 <style lang="scss"></style>
